3d5e54c7b9d88e96969e7c906aa1c2455718f2e4
Oumismoussa <124581649+Oumismoussa@users.noreply.github.com>
2023-03-15 16:32:20 +0100

    Update vibrate.js

--- vibrate.js
+++ vibrate.js
@@ -1,7 +1,12 @@
+
+let vibrateS = document.querySelector('.button1')
+let vibrateP = document.querySelector('.button2')
+
 function vibrate(ms){
     navigator.vibrate(ms);
 }
 
-function vibratePattern(){
+vibrateP.addEventListener('click',function vibratePattern(){
+    console.log('coucou')
     navigator.vibrate([300,100,300,100,300]);
-}
+})
