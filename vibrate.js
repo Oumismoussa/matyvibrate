1fd9f1e924ed70fbf77294ea5b108b2b747ec657
Oumismoussa <124581649+Oumismoussa@users.noreply.github.com>
2023-03-16 10:29:00 +0100

    Update vibrate.js

--- vibrate.js
+++ vibrate.js
@@ -6,13 +6,11 @@ if (navigator.vibrate){
     paragraphe.innerText = 'Ca ne marche pas'
 }
 
-let vibrateS = document.querySelector('.button1')
-let vibrateP = document.querySelector('.button2')
-
 function vibrate(ms){
     navigator.vibrate(ms);
 }
 
-vibrateP.addEventListener('click',function startVibrate(){ 
-navigator.vibrate([300,100,300,100])}
-);
+function startVibrate(){ 
+navigator.vibrate([300,100,300,100])
+}
+;
